fe5ae31153468dd87c14bea57a7041dc1c362cec
Jake Berg <jacobvictorberg@gmail.com>
2018-02-28 20:45:42 -0500

    game is now working with winner

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
+let tempDisk;
+let rod3 = document.getElementById("rod3")
+var main = document.querySelector("main");
 
 
+function createDisks() {
+    var destination = document.getElementById("rod1");
+    for (i = 4; i > 0; i--) {
+        let div = document.createElement("div");
+        div.id = (String(i));
+        div.className = ("disk");
+        destination.appendChild(div);
+    }
+}
+createDisks();
+
+function createHandler() {
+    var disks = document.getElementsByClassName("disk")
+    for (let i = 0; i < disks.length; i++) {
+        let disk = disks[i];
+        disk.addEventListener('click', selectDisk);
+    }
+}
+createHandler()
+
+function selectDisk(event) {
+    let thisDisk = event.target;
+    let idNum = thisDisk.id;
+    tempDisk = thisDisk;
+    console.log(tempDisk)
+}
+
+function placeDisk(event) {
+    let thisButton = event.target;
+    let parent = thisButton.parentElement;
+    tempId = tempDisk.id;
+    let topDisk = parent.lastElementChild.id;
+    if (tempId == 1) {
+        parent.appendChild(tempDisk);
+    } else if (tempId < topDisk || topDisk == false){
+        parent.appendChild(tempDisk);
+    }
+
+    if (rod3.childElementCount == 5){
+        alert("You win!");
+    }
+}
 
 // Click event for choices.
-var columns = document.querySelectorAll(".column");
+var columns = document.querySelectorAll("button");
 
 for (let i = 0; i < columns.length; i++) {
     let column = columns[i];
     column.addEventListener('click', placeDisk);
 }
-
-var d1 = document.getElementById("disk1");
-d1.addEventListener('click', selectDisk);
-
-var d2 = document.getElementById("disk2");
-d2.addEventListener('click', selectDisk);
-
-var d3 = document.getElementById("disk3");
-d3.addEventListener('click', selectDisk);
-
-var d4 = document.getElementById("disk4");
-d4.addEventListener('click', selectDisk);
